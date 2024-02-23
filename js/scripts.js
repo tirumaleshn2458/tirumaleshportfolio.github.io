250efcd9f00078b5993b1b38f4e9e35e25b5d9a3
tirumaleshn2458 <tirumaleshn2000@gmail.com>
2024-02-23 02:45:40 -0600

    udpated my portfolio with all the udpates from years after my graduation in bachlors

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,24 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+function toggleExpand(element) {
+    element.classList.toggle("expanded");
+  }
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
